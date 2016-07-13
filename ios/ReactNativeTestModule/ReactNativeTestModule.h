1334bb4922e728df214799821e2e62e47365bf46
huangpeng <huangpeng@chunyu.me>
2016-07-13 12:53:58 +0800

    init

---
+++ ios/ReactNativeTestModule/ReactNativeTestModule.h
@@ -0,0 +1,16 @@
+//
+//  ReactNativeTestModule.h
+//  ReactNativeTestModule
+//
+//  Created by HuangPeng on 7/13/16.
+//  Copyright © 2016 Chunyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Foundation/Foundation.h>
+#import <RCTBridgeModule.h>
+
+@interface ReactNativeTestModule : NSObject <RCTBridgeModule>
+
+@end
