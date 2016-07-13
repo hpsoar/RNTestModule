f5f7909592ef6e396d49dc9dafa9547d35e62a9b
huangpeng <huangpeng@chunyu.me>
2016-07-13 14:03:19 +0800

    fix project

--- ios/ReactNativeTestModule/ReactNativeTestModule.h
+++ ios/ReactNativeTestModule/ReactNativeTestModule.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 #import <Foundation/Foundation.h>
-#import <RCTBridgeModule.h>
+#import "RCTBridgeModule.h"
 
 @interface ReactNativeTestModule : NSObject <RCTBridgeModule>
 
